6573db40cbbca64df573fd3aaf7713d13bfa3fc7
NateRobinson <15951084423@163.com>
2015-11-16 17:29:01 +0800

    第一次提交。

---
+++ base-libary/src/main/java/com/gu/baselibrary/baseui/BaseApplication.java
@@ -0,0 +1,52 @@
+package com.gu.baselibrary.baseui;
+
+import android.app.Application;
+import android.os.StrictMode;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import cat.ereza.customactivityoncrash.CustomActivityOnCrash;
+
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.GINGERBREAD;
+
+/**
+ * Created by Nate on 2015/9/29.
+ */
+public class BaseApplication extends Application {
+
+    public static BaseApplication mInstance = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        enabledStrictMode();
+        CustomActivityOnCrash.install(this);
+        mInstance = this;
+        //创建默认的ImageLoader配置参数
+        ImageLoaderConfiguration configuration = ImageLoaderConfiguration.createDefault(this);
+        //Initialize ImageLoader with configuration.
+        ImageLoader.getInstance().init(configuration);
+    }
+
+    /**
+     * 开发阶段开启严苛模式
+     */
+    private void enabledStrictMode() {
+        if (SDK_INT >= GINGERBREAD) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder() //
+                    .detectAll() //
+                    .penaltyLog() //
+//                    .penaltyDeath() //
+                    .build());
+        }
+    }
+
+    /**
+     * @return BaseApplication
+     */
+    public static BaseApplication getInstance() {
+        return mInstance;
+    }
+}
